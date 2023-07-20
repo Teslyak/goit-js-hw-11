773ca51cee269be733a8c1ba99eaea742786bbe7
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-07-20 12:02:57 +0300

    hw11-css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { getQuery } from "./api-pixabay";
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
     form: document.querySelector('#search-form'),
@@ -13,13 +15,15 @@ refs.form.addEventListener('submit', onSubmit);
 async function onSubmit(event) {
     event.preventDefault();
 try {
-    const response = await getQuery(refs.input.value)
+  const response = await getQuery(refs.input.value)
   
-    console.log(response);
     if (!response.data.total) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
     };
-    await makeCard(response.data.hits)
+  await makeCard(response.data.hits)
+
+  const gallery = new SimpleLightbox('.gallery a');
+  
 } catch (error) {
    Notiflix.Notify.failure('Sorry, error get data. Please try again.');
 }
@@ -27,22 +31,25 @@ try {
  
 function makeCard(arr) {
     return arr.forEach(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => { 
-   const markUp = `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      const markUp = `<div class="photo-card">
+    <a class="card-link "href="${largeImageURL}" alt="${tags}" >
+  <img src="${webformatURL}" alt="${tags}" loading="lazy"  width = 300 height = 200/>
+  
   <div class="info">
     <p class="info-item">
-      <b>Likes:${likes}</b>
+      <b>Likes ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views:${views}</b>
+      <b>Views ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments:${comments} </b>
+      <b>Comments ${comments} </b>
     </p>
     <p class="info-item">
-      <b>Downloads:${downloads}</b>
+      <b>Downloads ${downloads}</b>
     </p>
   </div>
+  </a>
 </div>` 
         refs.gallery.insertAdjacentHTML('beforeend', markUp);
     })
